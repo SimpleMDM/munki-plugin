26cdbc8b896927018b378eff1a1d7ad7da369ad7
Taylor Boyko <taylor@wrprojects.com>
2020-09-10 16:35:49 -0700

    wip

--- build.sh
+++ build.sh
@@ -10,27 +10,29 @@ src_dir="$project_root/src"
 files_dir="$src_dir/files"
 pkg_path="${build_dir}/simplemdm-munki-plugin-${version}.pkg"
 
-rm -rf "$src_dir"
+rm -rf $src_dir
 
-mkdir -p "$files_dir/usr/local/munki/munkilib/munkirepo"
-cp "$project_root/SimpleMDMRepo.py" "$files_dir/usr/local/munki/munkilib/munkirepo/SimpleMDMRepo.py"
+mkdir -p $files_dir/usr/local/munki/munkilib/munkirepo
+cp $project_root/SimpleMDMRepo.py $files_dir/usr/local/munki/munkilib/munkirepo/SimpleMDMRepo.py
 
-mkdir -p "$files_dir/usr/local/simplemdm/munki-plugin"
-cp "$project_root/config.plist.example" "$files_dir/usr/local/simplemdm/munki-plugin/config.plist.example"
-chmod 666 "$files_dir/usr/local/simplemdm/munki-plugin/config.plist.example"
+mkdir -p $files_dir/usr/local/simplemdm/munki-plugin
+chmod 777 $files_dir/usr/local/simplemdm/munki-plugin
+cp $project_root/config.plist.example $files_dir/usr/local/simplemdm/munki-plugin/config.plist.example
+chmod 666 $files_dir/usr/local/simplemdm/munki-plugin/config.plist.example
 
-rm -rf "$build_dir"
-mkdir "$build_dir"
+rm -rf $build_dir
+mkdir $build_dir
 
-mkdir "$src_dir/scripts"
+mkdir $src_dir/scripts
 echo "#!/bin/bash
 cp -np /usr/local/simplemdm/munki-plugin/config.plist.example /usr/local/simplemdm/munki-plugin/config.plist
 exit 0" > $src_dir/scripts/postinstall
-chmod 777 "$src_dir/scripts/postinstall"
+chmod 777 $src_dir/scripts/postinstall
 
-pkgbuild --root "$files_dir" \
+pkgbuild --root $files_dir \
          --identifier com.simplemdm.munki_plugin \
-         --version $version "$pkg_path" \
-         --scripts "$src_dir/scripts"
+         --version $version \
+         --scripts $src_dir/scripts \
+         $pkg_path
 
-rm -rf "$src_dir"
+rm -rf $src_dir
